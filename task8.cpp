2c415c2560aee3d05c6b955deb30014b8bea0042
LotusBro98 <LotusBro98@yandex.ru>
2021-10-07 15:45:40 +0300

    Update task8.cpp

--- task8.cpp
+++ task8.cpp
@@ -7,25 +7,49 @@
 #include <vector>
 #include <cmath>
 
-int parse_args(int argc, char * argv[], uint64_t * N) {
-    if (argc != 2) {
-        printf("Wrong number of arguments! Expected 1 number\n");
+#define N_MAX 10000
+
+int get_array(int argc, char * argv[], int32_t** array, int * N) {
+    FILE * source;
+    if (argc == 1) {
+        source = stdout;
+    } else if (argc == 2) {
+        source = fopen(argv[1], "rt");
+        if (source == nullptr) {
+            printf("Failed to open file %s\n", argv[1]);
+            return -1;
+        }
+    } else {
+        printf("Wrong number of arguments! Required 1 filename or 0 (read from stdin)\n");
         return -1;
     }
 
-    char * pend;
-    *N = strtoull(argv[1], &pend, 10);
-    if (pend - argv[1] != strlen(argv[1])) {
-        printf("Wrong argument: %s\nExpected number\n");
+    int n;
+    fscanf(source, "%d", &n);
+    if (n <= 0 || n > N_MAX) {
+        printf("N out of bounds\n");
         return -1;
     }
 
+    int32_t * arr = new int32_t [n];
+    for (int i = 0; i < n; i++) {
+        if (fscanf(source, "%ld", &arr[i]) != 1) {
+            printf("Failed to read array element %d\n", i);
+            delete[] arr;
+            return -1;
+        }
+    }
+
+    *array = arr;
+    *N = n;
     return 0;
 }
 
 int main(int argc, char * argv[]) {
-    uint64_t N;
-    if (parse_args(argc, argv, &N))
+    int32_t * array;
+    int N;
+
+    if (get_array(argc, argv, &array, &N))
         return -1;
 
 
